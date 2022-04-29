cbd549e063a6b7d16f5c92b3204e40f307d0c265
aloprui26-06 <102860876+aloprui26-06@users.noreply.github.com>
2022-04-29 16:06:11 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.forever(function () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
+    } else {
+        maqueen.motorStop(maqueen.Motors.All)
+        basic.pause(1000)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 20)
+        basic.pause(2000)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 20)
+        basic.pause(1000)
+    }
+})
